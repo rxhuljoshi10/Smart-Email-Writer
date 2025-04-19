dfcca8c88ff5803e70ce3bdf098f34e995624ea9
Rahul Joshi <joshirahul7040@gmail.com>
2025-04-19 21:27:22 +0530

    Frontent UI Update : Tabs Integration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,32 @@
 import { useState } from 'react'
 import Container from '@mui/material/Container';
-import { Box, Button, AppBar, Toolbar, CircularProgress, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { Box, Button,CircularProgress, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import Slider from '@mui/material/Slider';
 import Card from '@mui/material/Card';
 import LabeledTextarea from './components/LabeledTextArea.jsx';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab from '@mui/joy/Tab';
+import Stack from '@mui/joy/Stack';
 
 
 function App() {
-  const[emailContent, setEmailContent] = useState("");
-  const[tone, setTone] = useState("");
-  const [replyLength, setReplyLength] = useState();
-  const[intent, setIntent] = useState("");
-  const[generatedReply, setGeneratedReply] = useState("");
-  const[loading, setLoading] = useState(false);
-  const[modifyLoading, setModifyLoading] = useState(false);
-  const[error, setError] = useState("");
+  const [emailContent, setEmailContent] = useState("");
+  const [tone, setTone] = useState("");
+  const [replyLength, setReplyLength] = useState(3);
+  const [intent, setIntent] = useState("");
+  const [generatedReply, setGeneratedReply] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [modifyLoading, setModifyLoading] = useState(false);
+  const [error, setError] = useState("");
   const [replyHistory, setReplyHistory] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [customKeywords, setCustomKeywords] = useState("");
   const [format, setFormat] = useState("");
+  const [tabIndex, setTabIndex] = useState(0);
+
+
   const MAX = 15;
   const MIN = 1;
   const marks = [
@@ -44,6 +51,7 @@ function App() {
           return newHistory;
         });
       }
+      setTabIndex(1);
     } catch (error) {
       setError("Failed to generate email reply. Please try again!");
       console.error(error);
@@ -111,7 +119,7 @@ function App() {
   
   return (
     <>
-    <Box sx={{ py: 3, borderRadius: 2, my: 4 }}>
+    <Box sx={{ py: 3, borderRadius: 2, my: 2 }}>
       <Typography 
         variant='h2' 
         component="h1" 
@@ -126,135 +134,161 @@ function App() {
     </Box>
 
     <Container sx={{width:'950px'}}>
-      <Card 
-        sx={{
-          mb:6,
-          boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.1)', // soft shadow
-          borderRadius: '20px', // rounded corners
-          padding: 3, // spacing inside
-          transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-          '&:hover': {
-            transform: 'translateY(-5px)', // slight lift on hover
-            boxShadow: '0px 12px 32px rgba(0, 0, 0, 0.2)', // stronger shadow
-          }
-        }}>
 
-        <LabeledTextarea
-          label="Email Content"
-          placeholder="Paste the email here to get response..."
-          value={emailContent}
-          onChange={(e) => setEmailContent(e.target.value)}
-        />
-
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, my: 2 }}>
-          <FormControl sx={{mb: 2, mr:2, width: '250px'}}>
-            <InputLabel>Tone (Optional)</InputLabel>
-            <Select
-              value={tone || ''}
-              label={"Tone (Optional)"}
-              onChange={(e) => setTone(e.target.value)}>
-                <MenuItem value="">None</MenuItem>
-                <MenuItem value="professional">Professional</MenuItem>
-                <MenuItem value="casual">Casual</MenuItem>
-                <MenuItem value="friendly">Friendly</MenuItem>
-            </Select>
-          </FormControl>
-
-          {/* <FormControl sx={{mb: 2, mr:2, width: '250px'}}>
-            <InputLabel>Length</InputLabel>
-            <Select
-              value={length || ''}
-              label={"Length"}
-              onChange={(e) => setLength(e.target.value)}>
-                <MenuItem value="">Default</MenuItem>
-                <MenuItem value="1 line">Short (1 line)</MenuItem>
-                <MenuItem value="2-3 line">Medium (2-3 line)</MenuItem>
-                <MenuItem value="5-7 line">Long (5-7 line)</MenuItem>
-            </Select>
-          </FormControl> */}
-
-          <FormControl sx={{mb: 2, width: '250px'}}>
-            <InputLabel>Intent</InputLabel>
-            <Select
-              value={intent || ''}
-              label={"Intent"}
-              onChange={(e) => setIntent(e.target.value)}>
-                <MenuItem value="">None</MenuItem>
-                <MenuItem value="confirm">Confirming</MenuItem>
-                <MenuItem value="decline">Declining</MenuItem>
-                <MenuItem value="agree">Agreeing</MenuItem>
-                <MenuItem value="clarification">Asking for Clarification</MenuItem>
-                <MenuItem value="update">Giving an Update</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl sx={{mb: 2, width: '250px'}}>
-            <InputLabel>Format</InputLabel>
-            <Select
-              value={format || ''}
-              label={"Format"}
-              onChange={(e) => setFormat(e.target.value)}>
-                <MenuItem value="">None</MenuItem>
-                <MenuItem value="paragraph">Paragraph</MenuItem>
-                <MenuItem value="bullets">Bullet Points</MenuItem>
-                <MenuItem value="template">Full Email Template</MenuItem>
-            </Select>
-          </FormControl>
-
-          <Box sx={{ width: 400 }}>
-            <Typography variant="subtitle2" gutterBottom>
-              Length Preference
-            </Typography>
-            <Slider
-              marks={marks}
-              step={1}
-              value={replyLength}
-              valueLabelDisplay="auto"
-              min={MIN}
-              max={MAX}
-              onChange={(_, newValue) => setReplyLength(newValue)}
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography
-                variant="body2"
-                sx={{ cursor: 'pointer' }}>
-                {MIN} Min
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{ cursor: 'pointer' }}>
-                {MAX} Max
-              </Typography>
-            </Box>
-          </Box>
-
-          <TextField
-            label="Preferred Keywords (Optional)"
-            variant="outlined"
-            fullWidth
-            value={customKeywords}
-            onChange={(e) => setCustomKeywords(e.target.value)}
-            helperText="Separate keywords by commas"
-          />
-        </Box>
-    
-        <Button
-          variant='contained'
-          onClick={handleSubmit}
-          disabled={!emailContent || loading}
-          fullWidth
-          sx={{height:'50px'}}>
-          {loading ? <CircularProgress size={24}/> : "Generate Reply"}
-        </Button>
+      <Stack spacing={2} 
+      sx={{
+        display: 'flex',
+        alignItems: 'center', // Center on Y-axis (optional)
         
-        {error && (
-          <Typography color='error' sx={{mb:2}}>
-          {error}
-        </Typography>
-        )}
-      </Card>
+      }}>
+        
+        <Tabs
+          aria-label="Email Tabs"
+          value={tabIndex}
+          onChange={(event, newValue) => setTabIndex(newValue)}
+        >
+          <TabList>
+            <Tab indicatorInset> Email Content </Tab>
+            <Tab indicatorInset> Generated Reply </Tab>
+          </TabList>
+        </Tabs>
+      </Stack>
 
-      {generatedReply && (
+      <Box sx={{my: 4}}>
+        {tabIndex === 0 && (
+        <Card 
+          sx={{
+            boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.1)', // soft shadow
+            borderRadius: '20px', // rounded corners
+            padding: 3, // spacing inside
+            transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+            '&:hover': {
+              transform: 'translateY(-5px)', // slight lift on hover
+              boxShadow: '0px 12px 32px rgba(0, 0, 0, 0.2)', // stronger shadow
+            }
+          }}>
+
+          <LabeledTextarea
+            label="Email Content"
+            placeholder="Paste the email here to get response..."
+            value={emailContent}
+            onChange={(e) => setEmailContent(e.target.value)}
+          />
+
+          <Typography variant="subtitle1" sx={{fontSize:16, fontWeight: '10'}}>
+            Optional Fields : 
+          </Typography>
+
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, mb: 4, p:2,  border: '1px solid grey' , borderRadius: '12px'}}>
+            <FormControl sx={{mb: 2, mr:2, width: '250px'}}>
+              <InputLabel>Tone (Optional)</InputLabel>
+              <Select
+                value={tone || ''}
+                label={"Tone (Optional)"}
+                onChange={(e) => setTone(e.target.value)}>
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="professional">Professional</MenuItem>
+                  <MenuItem value="casual">Casual</MenuItem>
+                  <MenuItem value="friendly">Friendly</MenuItem>
+              </Select>
+            </FormControl>
+
+            {/* <FormControl sx={{mb: 2, mr:2, width: '250px'}}>
+              <InputLabel>Length</InputLabel>
+              <Select
+                value={length || ''}
+                label={"Length"}
+                onChange={(e) => setLength(e.target.value)}>
+                  <MenuItem value="">Default</MenuItem>
+                  <MenuItem value="1 line">Short (1 line)</MenuItem>
+                  <MenuItem value="2-3 line">Medium (2-3 line)</MenuItem>
+                  <MenuItem value="5-7 line">Long (5-7 line)</MenuItem>
+              </Select>
+            </FormControl> */}
+
+            <FormControl sx={{mb: 2, width: '250px'}}>
+              <InputLabel>Intent</InputLabel>
+              <Select
+                value={intent || ''}
+                label={"Intent"}
+                onChange={(e) => setIntent(e.target.value)}>
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="confirm">Confirming</MenuItem>
+                  <MenuItem value="decline">Declining</MenuItem>
+                  <MenuItem value="agree">Agreeing</MenuItem>
+                  <MenuItem value="clarification">Asking for Clarification</MenuItem>
+                  <MenuItem value="update">Giving an Update</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{mb: 2, width: '250px'}}>
+              <InputLabel>Format</InputLabel>
+              <Select
+                value={format || ''}
+                label={"Format"}
+                onChange={(e) => setFormat(e.target.value)}>
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="paragraph">Paragraph</MenuItem>
+                  <MenuItem value="bullets">Bullet Points</MenuItem>
+                  <MenuItem value="template">Full Email Template</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Box sx={{ width: 400 }}>
+              <Typography variant="subtitle2" gutterBottom>
+                Length Preference
+              </Typography>
+              <Slider
+                marks={marks}
+                step={1}
+                value={replyLength}
+                valueLabelDisplay="auto"
+                min={MIN}
+                max={MAX}
+                onChange={(_, newValue) => setReplyLength(newValue)}
+              />
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography
+                  variant="body2"
+                  sx={{ cursor: 'pointer' }}>
+                  {MIN} Min
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ cursor: 'pointer' }}>
+                  {MAX} Max
+                </Typography>
+              </Box>
+            </Box>
+
+            <TextField
+              label="Preferred Keywords (Optional)"
+              variant="outlined"
+              fullWidth
+              value={customKeywords}
+              onChange={(e) => setCustomKeywords(e.target.value)}
+              helperText="Separate keywords by commas"
+            />
+          </Box>
+      
+          <Button
+            variant='contained'
+            onClick={handleSubmit}
+            disabled={!emailContent || loading}
+            fullWidth
+            sx={{height:'50px', borderRadius:3}}>
+            {loading ? <CircularProgress size={24}/> : "Generate Reply"}
+          </Button>
+          
+          {error && (
+            <Typography color='error' sx={{mb:2}}>
+            {error}
+          </Typography>
+          )}
+        </Card>
+        )}
+
+        {tabIndex === 1 &&(
         <Card 
           sx={{
             boxShadow: '0px 8px 24px rgba(0, 0, 0, 0.1)', // soft shadow
@@ -274,67 +308,51 @@ function App() {
             onChange={(e) => setGeneratedReply(e.target.value)}
           />
 
-          <Box sx={{ mt:5}}>
-            {/* <Typography variant='h6' gutterBottom>
-              Generated Reply
-            </Typography> */}
+          <Box sx={{ display: 'flex', alignItems: 'center' , position: 'relative'}}>
+            <Button onClick={handleLeftArrow} disabled={currentIndex <= 0} sx={{ minWidth: '40px' }}>
+              ⬅️
+            </Button>
 
-            {/* <Box sx={{ position: 'relative', width: '100%' }}> */}
-              <Box sx={{ display: 'flex', alignItems: 'center' , position: 'relative'}}>
-                <Button onClick={handleLeftArrow} disabled={currentIndex <= 0} sx={{ minWidth: '40px' }}>
-                  ⬅️
-                </Button>
-{/* 
-                <TextField
-                  fullWidth
-                  multiline
-                  rows={6}
-                  variant='outlined'
-                  value={generatedReply || ""}
-                  onChange={(e) => setGeneratedReply(e.target.value)}
-                  inputProps={{ "aria-readonly": true }}
-                /> */}
+            <Button onClick={handleRightArrow} disabled={loading} sx={{ minWidth: '40px' }}>
+              ➡️
+            </Button>
 
-                <Button onClick={handleRightArrow} disabled={loading} sx={{ minWidth: '40px' }}>
-                  ➡️
-                </Button>
-
-                {modifyLoading && (
-                <CircularProgress
-                  size={20}
-                  sx={{
-                    position: 'absolute',
-                    top: 14,
-                    right: 50,
-                    // zIndex: 1,
-                  }}
-                />)}
-              </Box>
-            
-
-            {generatedReply && ( 
-            <Box sx={{mt: 2}}>
-              <Button variant='outlined' sx={{mr: 3}}
-                onClick={() => handleModification("Expand")}
-                disabled={modifyLoading} >
-                  Expand!
-              </Button>
-
-              <Button variant='outlined' sx={{mr: 3}}
-                onClick={() => handleModification("Shorten")}
-                disabled={modifyLoading} >
-                  Shorten!
-              </Button>
-
-              <Button variant='outlined'
-                onClick={() => navigator.clipboard.writeText(generatedReply)}>
-                Copy to Clipboard
-              </Button>
-            </Box>
-            )}
+            {modifyLoading && (
+            <CircularProgress
+              size={20}
+              sx={{
+                position: 'absolute',
+                top: 14,
+                right: 50,
+                // zIndex: 1,
+              }}
+            />)}
           </Box>
+            
+          <Box sx={{mt: 2}}>
+            <Button variant='outlined' sx={{mr: 3}}
+              onClick={() => handleModification("Expand")}
+              disabled={modifyLoading} >
+                Expand!
+            </Button>
+
+            <Button variant='outlined' sx={{mr: 3}}
+              onClick={() => handleModification("Shorten")}
+              disabled={modifyLoading} >
+                Shorten!
+            </Button>
+
+            <Button variant='outlined'
+              onClick={() => navigator.clipboard.writeText(generatedReply)}>
+              Copy to Clipboard
+            </Button>
+          </Box>
+          
         </Card>
-      )}
+        )}
+      </Box>
+      
+
     </Container>
     </>
   )
